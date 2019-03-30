95a2c1bb77e8e8e49f79059ca83a476305ad9c3f
aditya-garg-19 <qadityag@gmail.com>
2019-03-30 18:06:09 +0530

    making multiple operation with multiple params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { templateJitUrl } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -24,23 +25,167 @@ export class AppComponent {
   num1;
   num2;
   operation;
+  countAdd=0;
+  countSub =0;
+  countMulti = 0;
+  countDevide = 0;
 
     valuePass(num1,num2,operation) {
-      this.result = num1;
-      if(operation == "add")
+      if(num2 == undefined && operation == undefined)
+      {
+      this.result = num1 + this.result*10;
+      }       
+          
+       if(operation == "add")
       {
         this.result = num1 + num2;
+      }
+       if(operation == "minus")
+      {
+        this.result = num1 - num2;
+      }
+      if(operation == "multiply")
+      {
+        this.result = num1 * num2;
+      }
+      if(operation == "devide")
+      {
+        this.result = num1 / num2;
       }
     }
     
     add(){
+      this.countAdd++;
+      this.num2 = this.result;
+      
+      console.log(this.countAdd);
+      if(this.countAdd > 1)
+      {
+        this.num1 = this.num1 + this.result;
+      }
+     else if(this.operation == 'minus')
+      {
+          this.num1 = this.num1 - this.result;
+      }
+      else if(this.operation == 'multiply')
+      {
+          this.num1 = this.num1 * this.result;
+      }
+      else if(this.operation == 'devide')
+      {
+          this.num1 = this.num1 / this.result;
+      }
+      else
+      {
       this.num1 = this.result;
-      this.operation = "add";
+      
+      }
+      this.operation = "add"
+      this.result = 0;
     }
 
+    minus(){
+      this.countSub++;
+      this.num2 = this.result;
+      
+      console.log(this.countSub);
+      if(this.countSub > 1)
+      {
+        this.num1 = this.num1 - this.result;
+      }
+      else if(this.operation == 'add')
+      {
+          this.num1 = this.num1 + this.result;
+      }
+      else if(this.operation == 'multiply')
+      {
+          this.num1 = this.num1 * this.result;
+      }
+      else if(this.operation == 'devide')
+      {
+          this.num1 = this.num1 / this.result;
+      }
+      else
+      {
+      this.num1 = this.result;
+      
+      }
+      this.operation = "minus";
+      this.result = 0;
+    }
+
+
+    multiply(){
+      this.countMulti++;
+      this.num2 = this.result;
+      
+      console.log(this.countMulti);
+      if(this.countMulti > 1)
+      {
+        this.num1 = this.num1 - this.result;
+      }
+      else if(this.operation == 'add')
+      {
+          this.num1 = this.num1 + this.result;
+      }
+      else if(this.operation == 'minus')
+      {
+          this.num1 = this.num1 - this.result;
+      }
+      else if(this.operation == 'devide')
+      {
+          this.num1 = this.num1 / this.result;
+      }
+      else
+      {
+      this.num1 = this.result;
+      
+      }
+      this.operation = "multiply";
+      this.result = 0;
+    }
+
+
+    devide(){
+      this.countDevide++;
+      this.num2 = this.result;
+      
+      console.log(this.countDevide);
+      if(this.countDevide > 1)
+      {
+        this.num1 = this.num1 - this.result;
+      }
+      else if(this.operation == 'add')
+      {
+          this.num1 = this.num1 + this.result;
+      }
+      else if(this.operation == 'minus')
+      {
+          this.num1 = this.num1 - this.result;
+      }
+      else if(this.operation == 'multiply')
+      {
+          this.num1 = this.num1 * this.result;
+      }
+      else
+      {
+      this.num1 = this.result;
+      
+      }
+      this.operation = "devide";
+      this.result = 0;
+    }
+
+
     equals(){
+      this.countSub = 0;
+      this.countAdd = 0;
+      this.countMulti = 0;
+      this.countDevide = 0;
+      
       this.num2 = this.result
-      this.valuePass(this.num1,this.num2, this.operation)
+      this.valuePass(this.num1,this.num2, this.operation);
+      this.operation = '';
     }
 
     clear(){
